fbe60b7caaf88e0e7754fe40303fa85b4eeca955
Som3sh <someshmohanty08@gmail.com>
2026-01-07 17:45:14 +0530

    7 JAN 2025

--- Self/practice.sql
+++ Self/practice.sql
@@ -1,6 +1,133 @@
-SELECT TOP (1000) [id] 
-        , [first_name]
-        ,[country]
-        ,[score]
-    FROM [MyDatabase].[dbo].[customers]
+-- SELECT TOP (1000) [id] 
+--         , [first_name]
+--         ,[country]
+--         ,[score]
+--     FROM [MyDatabase].[dbo].[customers]
 
+
+-- Retrieve all data from the customer table 
+SELECT *
+FROM customers
+
+
+-- Retrieve all the order data
+
+SELECT * 
+FROM orders
+
+
+-- Retrieve each customers name , country and score
+
+SELECT 
+    first_name,
+    Country ,
+    score
+FROM customers
+
+
+-- Retrieve the customers whose score is not equal to 0
+
+SELECT *
+FROM customers
+WHERE score>0
+
+-- or --
+
+SELECT *
+FROM customers
+WHERE score!=0
+
+
+-- Retrieve customers from Germany 
+
+SELECT *
+FROM customers
+WHERE country='Germany'
+
+SELECT 
+    first_name,
+    country
+FROM customers
+WHERE country='Germany'
+
+-- Retrieve all customers and sort all the results by highest score first
+
+SELECT *
+FROM customers
+ORDER BY score DESC
+
+-- Retrieve all customers and sort all the results by lowest score first
+
+SELECT *
+FROM customers
+ORDER BY score ASC
+
+
+
+-- Retrieve all customers and sort the results by the country and then by the highest score
+
+SELECT *
+FROM customers
+ORDER BY 
+    country ASC,
+    score DESC
+
+-- Find the total score for each country
+
+SELECT 
+    country,
+    SUM(score) AS aggregate_score
+FROM customers
+GROUP BY country 
+
+-- Find the total score and the total no. of customers from each country
+
+SELECT 
+    country,
+    SUM(score) AS aggregate_score,
+    count(id) AS total_customers
+FROM customers
+GROUP BY country
+
+
+-- Find the average score for each country considering only customers 
+-- with a score not equal to 0 and return only those countries with an average score greater than 430
+
+SELECT 
+    country,
+    AVG(score) AS Average_Score
+FROM customers
+WHERE score!=0
+GROUP BY country
+HAVING AVG(score) > 430
+
+
+
+-- Return Unique list of all countries 
+
+SELECT DISTINCT 
+    country 
+FROM customers
+
+
+
+-- Retrieve only 3 customers
+
+
+SELECT TOP 3 *
+FROM customers
+
+
+-- Retrieve 3 customers with the highest scores 
+
+SELECT TOP 3 *
+FROM customers
+ORDER BY score DESC
+
+
+-- Retrieve 2 customers with the lowest scores 
+
+SELECT TOP 2 *
+FROM customers
+ORDER BY score ASC
+ 
